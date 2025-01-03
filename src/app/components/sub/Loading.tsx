4e39a845a94264eeb36297fe9f60fa5934548301
oggyyy2000 <tuanbeos47@gmail.com>
2025-01-03 09:34:41 +0700

    create loading page

---
+++ src/app/components/sub/Loading.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+
+const Loading = () => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+  }, []);
+
+  return (
+    <motion.div
+      initial={{ top: 0 }}
+      animate={{ top: loading ? "-100%" : 0 }}
+      transition={{ duration: 0.5 }}
+      className="w-full h-full fixed top-0 left-0 flex items-center justify-center 
+    bg-gradient-to-t from-yellow-50 to-red-50 z-50"
+    >
+      <Image src="/spinner.gif" width={40} height={40} alt="spinner loading" />
+    </motion.div>
+  );
+};
+
+export default Loading;
