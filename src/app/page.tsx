46ef2a3d5d9f8399a954a55a2257c68a60e79173
oggyyy2000 <tuanbeos47@gmail.com>
2025-01-03 09:38:10 +0700

    add toggle and loading component, add clean up function for useEffect hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
 "use client";
 import { useState, useRef, useEffect } from "react";
-import Hero from "./components/Hero";
-import About from "./components/About";
-import Experiences from "./components/Experiences";
-import Skills from "./components/Skills";
-import Project from "./components/Projects";
-import Contact from "./components/Contact";
-import Navbar from "./components/Navbar";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Experiences from "@/components/Experiences";
+import Skills from "@/components/Skills";
+import Project from "@/components/Projects";
+import Contact from "@/components/Contact";
+import Navbar from "@/components/Navbar";
+import Toggle from "@/components/sub/Toggle";
+import Loading from "@/components/sub/Loading";
 
 export default function Home() {
   const [id, setId] = useState("");
-  console.log("id: ", id);
   const compsRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -29,19 +30,29 @@ export default function Home() {
     compsArray.forEach((comp) => {
       observer.observe(comp);
     });
+
+    return () => {
+      compsArray.forEach((comp) => observer.unobserve(comp));
+      observer.disconnect();
+    };
   }, []);
 
   return (
     <>
-      <Navbar id={id} />
-      <div ref={compsRef}>
-        <Hero />
-        <About />
-        <Experiences />
-        <Skills />
-        <Project />
-        <Contact />
-      </div>
+      <Loading />
+      <Toggle>
+        <Navbar id={id} />
+        <main>
+          <div ref={compsRef}>
+            <Hero />
+            <About />
+            <Experiences />
+            <Skills />
+            <Project />
+            <Contact />
+          </div>
+        </main>
+      </Toggle>
     </>
   );
 }
